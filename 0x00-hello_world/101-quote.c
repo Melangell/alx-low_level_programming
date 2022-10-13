be86356e82fb51f7f8c4e960c94765ffa2d3f7ab
Melangell <veraonwuekwe@gmail.com>
2022-10-13 15:57:12 -0700

    Geniuses only

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - alternative output function
+ * Return: 1 if successful
+ */
+int main(void)
+{
+	echo"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	return (1);
+}
