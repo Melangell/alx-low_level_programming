6236ddb8acebfefe5045be8a2e5a9eab173ab56e
Melangell <veraonwuekwe@gmail.com>
2022-10-13 13:13:44 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-	//string initialisation
-	char Mystr[] = "Programming is like building a multilingual puzzle\n";
+	puts("Programming is like building a multilingual puzzle\n");
 
-	puts(Mystr); //writing the string to stdout
-	return(0);
+	return (0);
 }
 
