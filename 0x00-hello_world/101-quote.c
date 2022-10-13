7163d34bcbfa5b40fc399d684a9bde690c57442a
Melangell <veraonwuekwe@gmail.com>
2022-10-13 16:21:46 -0700

    Geniuses only

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - alternative output function
  * Return: 1 if successful
  */
 int main(void)
 {
-	echo"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int c;
+
+	c = write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
