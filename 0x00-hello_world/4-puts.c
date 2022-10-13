e84a69550c4f428f69a916a219c01c50109d8969
Melangell <veraonwuekwe@gmail.com>
2022-10-13 13:47:11 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - using puts format
- * return - 0 if positive
+ * return: 0 if positive
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
