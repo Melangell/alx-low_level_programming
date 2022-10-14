97e3fff1dfbbfbbd8d707b803fc8bb33009cdba8
Melangell <veraonwuekwe@gmail.com>
2022-10-14 16:00:31 -0700

    ascii

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -print numbers without using char as variable
+ * Return: 0 if successful
+i*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+{
+	putchar(n);
+}
+{
+	putchar('\n');
+	return (0);
+}
+}
