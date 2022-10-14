0b2665f239d17496a4a637b774c0502e9b63010d
Melangell <veraonwuekwe@gmail.com>
2022-10-14 16:51:25 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints in base 16
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n <= 57; n++)
+{
+	putchar(n);
+}
+	for (ch = 'a'; ch <= 'f'; ch++)
+{
+	putchar(ch);
+}
+{
+	putchar('\n');
+	return (0);
+}
+}
