efb153b0fa9e73b599f1bb8e0335a51badf00270
Melangell <veraonwuekwe@gmail.com>
2022-10-14 12:25:59 -0700

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints upper with lower cases
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int ch;
+{
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar (ch);
+}
+{
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	putchar (ch);
+	putchar ('\n');
+}
+	return (0);
+}
