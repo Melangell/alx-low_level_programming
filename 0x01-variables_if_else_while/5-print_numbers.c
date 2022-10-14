c32da7bd1f87ff66ce286254c355e01ccc43ad96
Melangell <veraonwuekwe@gmail.com>
2022-10-14 15:15:13 -0700

    Prints numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints number of base 10
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n == '0'; n <= n % 10; n++)
+{
+	printf("%d", n);
+}
+	putchar('\n');
+	return (0);
+}
