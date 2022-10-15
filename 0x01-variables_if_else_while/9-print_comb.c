c4ca5238785e5e87799db6a9a40f402048c187b7
Melangell <veraonwuekwe@gmail.com>
2022-10-14 18:19:01 -0700

    Not enough patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints digits with commas
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+{
+	putchar(n);
+	putchar(44);
+	putchar(32);
+}
+{
+	putchar('\n');
+	return (0);
+}
+}
