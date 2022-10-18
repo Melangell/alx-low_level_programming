408fb629d825242b5f9a7cc6db38ca7f56b91f27
Melangell <veraonwuekwe@gmail.com>
2022-10-18 10:23:46 -0700

    underscore putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints _putchar
  * Return: 0 if successful
  */
 int main(void)
 {
-	int n;
-
-	write(2, "_putchar\n", 8, n);
+	write(2, "_putchar\n", 9);
 	return (0);
 }
