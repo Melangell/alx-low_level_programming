650f29096e541d38d4ad792f799137fdbae8811a
Melangell <veraonwuekwe@gmail.com>
2022-10-18 13:05:53 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets
+ * Return: 0 if successful
+ */
+void print_alphabet(void)
+{
+	int _putchar(char c);
+	int i;
+
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+	_putchar('\n');
+	return;
+}
