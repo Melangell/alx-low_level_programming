b4274adb0ce0bc6fd8dc81b33f05a998c1ce8f68
Melangell <veraonwuekwe@gmail.com>
2022-10-20 10:38:45 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints x10 of certain numbers
+ * Return: no return
+ */
+void more_numbers(void)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+				_putchar(c / 10);
+			_putchar(c % 10);
+		}
+		_putchar('\n');
+	}
+}
