cf4666e507a4abff9a94d3ac70037b5371fc0e00
Melangell <veraonwuekwe@gmail.com>
2022-10-20 05:45:27 -0700

    print nummbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int _putchar(char c);
 void print_numbers(void);
 
 #endif
