68f4d7a009007d8f438c6e0c6ff822762407da60
Melangell <veraonwuekwe@gmail.com>
2022-10-20 06:07:35 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,9 @@ void print_numbers(void)
 {
 	int c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = 48; c <= 57; c++)
 	{
-		_putchar(c + '0');
-
-		_putchar('\n');
+		_putchar(c);
 	}
+	_putchar('\n');
 }
