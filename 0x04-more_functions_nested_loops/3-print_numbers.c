bcf4200a74a60e28581fc6afaac9b8a175902fbe
Melangell <veraonwuekwe@gmail.com>
2022-10-20 05:14:25 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0-9
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int c;
+	int _putchar(char c);
+
+	if (c >= 48 && c <= 57)
+		_putchar(c);
+	_putchar('\n');
+}
