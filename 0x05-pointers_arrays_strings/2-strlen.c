7c87447e47473435909becdac0734a933f1dc96b
Melangell <veraonwuekwe@gmail.com>
2022-10-25 13:21:15 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns length of a string
+ * @s: character to be verified
+ * Return: c if successful
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while(s[c] != '\0')
+	c++;
+
+	return c;
+}
