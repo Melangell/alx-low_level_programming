737debcd073e2e5379906c1d3cda5f1f25bcb1f3
Melangell <veraonwuekwe@gmail.com>
2022-11-04 16:05:46 +0000

    naming ceremony

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: argument that counts command line
+ * @argv: argument vector
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i <= 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
