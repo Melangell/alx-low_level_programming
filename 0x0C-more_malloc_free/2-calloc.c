ac38acbc66f1f80060033302036700bca479e693
Melangell <veraonwuekwe@gmail.com>
2022-11-10 23:49:10 +0000

    Task completed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,18 +11,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *ar;
 
-	for (i = 0; i < (nmemb * size); i++)
-	{
-		ar[i] = 0;
-	}
-	ar = malloc(nmemb * size);
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
+	ar = malloc(nmemb * size);
 	if (ar == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		ar[i] = 0;
 	}
 	return (ar);
 }
