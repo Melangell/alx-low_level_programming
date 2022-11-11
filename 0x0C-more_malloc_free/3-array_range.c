95a786f06b21ed90939091088b0a73be59cf5df3
Melangell <veraonwuekwe@gmail.com>
2022-11-11 00:27:19 +0000

    Task completed

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,17 +12,15 @@ int *array_range(int min, int max)
 	int i;
 
 	if (min > max)
-	{
 		return (NULL);
-	}
-	ar = malloc(sizeof(*ar) * ((min - max) + 1));
+
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
+
 	if (ar == NULL)
-	{
 		return (NULL);
-	}
+
 	for (i = 0; min <= max; i++, min++)
-	{
 		ar[i] = min;
-	}
+
 	return (ar);
 }
